a47d4b59e0b6c8dcf288b2a12e49f5fdcd1798ee
arsalon <arsalon47@gmail.com>
2018-05-01 14:25:13 -0400

    created scratch file

--- models/note.js
+++ models/note.js
@@ -1,2 +1,12 @@
 'use strict';
 
+const mongoose = require('mongoose');
+
+const noteSchema = mongoose.Schema({
+  title: {type: String, required: true},
+  content: {type: String},
+  timestamps: true
+});
+
+const Note = mongoose.model('Note', noteSchema);
+module.exports = {Note};
