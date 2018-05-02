75adb24764474a3643e6929cabafa12e3d8ef9ff
arsalon <arsalon47@gmail.com>
2018-05-02 12:53:14 -0400

    testing

--- config.js
+++ config.js
@@ -3,3 +3,5 @@
 exports.PORT = process.env.PORT || 8080;
 
 exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/noteful';
+
+exports.TEST_MONGODB_URI = process.env.TEST_MONGODB_URI || 'mongodb://localhost/noteful-test';
--- models/note.js
+++ models/note.js
@@ -10,5 +10,13 @@ const noteSchema = new Schema({
 {timestamps: true}
 );
 
+noteSchema.set('toObject', {
+  transform: function (doc, ret) {
+    ret.id = ret._id;
+    delete ret._id;
+    delete ret.__v;
+  }
+});
+
 const Note = mongoose.model('Note', noteSchema);
 module.exports = {Note};
